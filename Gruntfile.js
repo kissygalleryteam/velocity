4de40078769831907fe97dbdedea5d6490f8b420
shepherdwind <eward.song@gmail.com>
2013-04-10 10:59:10 +0800

    little change, rm file

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,26 +22,6 @@ module.exports = function(grunt) {
         } 
       }
     },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      }
-    },
     copy: {
       main: {
         files: [
@@ -57,7 +37,6 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task.
